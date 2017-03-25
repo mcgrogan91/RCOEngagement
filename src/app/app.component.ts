b9bd448a2aa046078eef3e694866bb5538b5b501
James Marshall <jamesomarshall@gmail.com>
2017-03-25 18:48:53 -0400

    Added bootstrap cdn into index. Added header to the My-app component as a starting point.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  	<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  	<a class="navbar-brand" href="#">ROC Engagement</a>
+	  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		    </ul>
+	  	</div>
+	</nav>`,
+
 })
 export class AppComponent  { name = 'Angular'; }
