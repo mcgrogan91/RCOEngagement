be6e753bfb99bec5feed01818e6d6cf27a106708
Kyle McGrogan <mcgrogan91@gmail.com>
2017-03-26 20:30:46 -0400

    Proof of concept for GPS API
    This will turn a Philadelphia address into a set of GPS coordinates using the AIS service.  A basic controller is built around it to allow a GET request with an address parameter to find coordinates.

---
+++ api/app/Http/Controllers/RCOSearchController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kmcgrogan
+ * Date: 3/26/17
+ * Time: 3:27 PM
+ */
+
+namespace App\Http\Controllers;
+
+use App\Services\AddressTranslationService;
+use Illuminate\Support\Facades\Input;
+
+class RCOSearchController extends Controller
+{
+
+    public function search()
+    {
+        $rcos = collect(['Hello', 'World']);
+
+        if ($address = Input::get('address')) {
+            try {
+                $coordinates = AddressTranslationService::getGPSFromAddress($address);
+            } catch (Exception $e) {
+                dd($e);
+            }
+            
+            return $coordinates;
+        }
+
+        return response()->json($rcos);
+    }
+}
