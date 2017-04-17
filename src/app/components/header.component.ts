ebfa42b89698308b3bf8743c8904375e629186db
Lisa MacCarrigan <lisa.maccarrigan@gmail.com>
2017-04-17 01:54:32 -0400

    Began to update the look and feel of the site based on the wireframes, added footer styles and initial content, updated header to include navbar-toggler, began to style home page

--- src/app/components/header.component.ts
+++ src/app/components/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'header',
   templateUrl: './header.html',
+  styleUrls: ['./header.css'],
 
 })
 export class HeaderComponent  {}
